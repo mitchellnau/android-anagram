2bb4bfe6ce6acc7550a858fb98c942bf05ff9141
mitchellnau <mmh327@nau.edu>
2016-10-08 13:40:04 -0700

    Change to about activity

--- Anagram/app/src/main/java/com/example/cs399/hewitt_m_kahn_a_anagram/MainActivity.java
+++ Anagram/app/src/main/java/com/example/cs399/hewitt_m_kahn_a_anagram/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.cs399.hewitt_m_kahn_a_anagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtLaunchActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtLaunchActivity = (Button) findViewById(R.id.button2);
+
+        mBtLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toAbout();
+            }
+        });
+    }
+
+    public void toAbout(){
+
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
