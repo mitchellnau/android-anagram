5d3abd365e65ad24829c85e437c4b80aba824a8a
mitchellnau <mmh327@nau.edu>
2016-10-08 14:38:01 -0700

    newgame activity

---
+++ Anagram/app/src/main/java/com/example/cs399/hewitt_m_kahn_a_anagram/NewGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs399.hewitt_m_kahn_a_anagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+}
