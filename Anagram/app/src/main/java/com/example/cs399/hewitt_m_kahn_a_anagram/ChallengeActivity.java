774aa5406b7ad646aecd5acf45075868d1bdb47a
mitchellnau <mmh327@nau.edu>
2016-10-09 14:39:46 -0700

    challenge and easy activities

---
+++ Anagram/app/src/main/java/com/example/cs399/hewitt_m_kahn_a_anagram/ChallengeActivity.java
@@ -0,0 +1,31 @@
+package com.example.cs399.hewitt_m_kahn_a_anagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class ChallengeActivity extends AppCompatActivity {
+
+    private Button easyButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+
+        easyButton = (Button) findViewById(R.id.easybutton);
+
+        easyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toEasy();
+            }
+        });
+    }
+
+    public void toEasy(){
+        Intent intent = new Intent(this, EasyActivity.class);
+        startActivity(intent);
+    }
+}
